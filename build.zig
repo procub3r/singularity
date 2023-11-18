f93238068120b1aa2793a790e914027e765f11be
procub3r <procub3r@proton.me>
2023-11-19 01:50:08 +0530

    Update build.zig to work with Zig 0.12
    It has been a while. It has been fun. Back on the osdev train(?!)

--- build.zig
+++ build.zig
@@ -1,41 +1,36 @@
 const std = @import("std");
-const CrossTarget = std.zig.CrossTarget;
-const Target = std.Target;
 
 pub fn build(b: *std.Build) void {
-    const target = CrossTarget{
-        .cpu_arch = Target.Cpu.Arch.x86_64,
+    // Target a freestanding environment which uses just the general purpose registers.
+    const target = std.zig.CrossTarget{
+        .cpu_arch = .x86_64,
         .cpu_features_add = blk: {
-            var add_features = Target.Cpu.Feature.Set.empty;
-            add_features.addFeature(@enumToInt(Target.x86.Feature.soft_float));
+            var add_features = std.Target.Cpu.Feature.Set.empty;
+            // TODO: add features.
             break :blk add_features;
         },
         .cpu_features_sub = blk: {
-            var sub_features = Target.Cpu.Feature.Set.empty;
-            sub_features.addFeature(@enumToInt(Target.x86.Feature.x87));
-            sub_features.addFeature(@enumToInt(Target.x86.Feature.mmx));
-            sub_features.addFeature(@enumToInt(Target.x86.Feature.sse));
-            sub_features.addFeature(@enumToInt(Target.x86.Feature.sse2));
-            sub_features.addFeature(@enumToInt(Target.x86.Feature.avx));
-            sub_features.addFeature(@enumToInt(Target.x86.Feature.avx2));
+            var sub_features = std.Target.Cpu.Feature.Set.empty;
+            // TODO: remove features.
             break :blk sub_features;
         },
-        .os_tag = Target.Os.Tag.freestanding,
-        .abi = Target.Abi.none,
+        .os_tag = std.Target.Os.Tag.freestanding,
+        .abi = std.Target.Abi.none,
     };
 
+    // Use the standard optimization options. I'll probably build using -Doptimize=ReleaseSmall
+    // because I haven't figured out how to make use of debug info yet.
     const optimize = b.standardOptimizeOption(.{});
+
+    // kernel elf executable
     const kernel = b.addExecutable(.{
         .name = "kernel.elf",
         .root_source_file = .{ .path = "kernel/main.zig" },
         .target = target,
         .optimize = optimize,
     });
-    kernel.setLinkerScriptPath(.{ .path = "./linker.ld" });
-    const copy_kernel = b.addInstallBinFile(kernel.getOutputSource(), "kernel.elf");
 
-    const build_kernel = b.step("kernel", "Build the kernel");
-    build_kernel.dependOn(&copy_kernel.step);
-
-    b.default_step = build_kernel;
+    // Use custom linker script to load to the higher half, among other things.
+    kernel.setLinkerScript(.{ .path = "linker.ld" });
+    b.installArtifact(kernel);
 }
