4b0bdef12261e96a83065b6033bd7f9fd11a34f6
procub3r <procub3r@proton.me>
2023-12-28 22:54:11 +0530

    Create GDT for long mode and load it
    Maybe I yeeted some of this code from the original version of
    singularity. Maybe I didn't.

--- bootloader/main.asm
+++ bootloader/main.asm
@@ -11,9 +11,37 @@ jmp 0x0000:init
 
 ; bootloader init
 init:
-    mov dx, 0xa6d2
-    call print_hex
-    jmp $
+    cli ; clear the IF flag to disable
+        ; maskable external interrupts
+
+    lgdt [gdtr] ; load gdt for long mode
+    jmp $ ; hang indefinitely
+
+; 64 bit global descriptor table
+gdt:
+    dq 0x00 ; first entry is a null descriptor
+
+    ; code segment descriptor
+    dw 0x0000     ; limit
+    dw 0x0000     ; base lo
+    db 0x00       ; base mid
+    db 0b10011010 ; access
+    db 0b10100000 ; flags and limit hi(?)
+    db 0x00       ; base hi
+
+    ; data segment descriptor
+    dw 0x0000     ; limit
+    dw 0x0000     ; base lo
+    db 0x00       ; base mid
+    db 0b10010010 ; access
+    db 0b11000000 ; flags and limit hi(?)
+    db 0x00       ; base hi
+.end:
+
+; gdt register
+gdtr:
+    dw gdt.end - gdt - 1 ; size of gdt
+    dq gdt ; pointer to gdt
 
 times 510 - ($ - $$) db 0
 dw 0xaa55 ; bootsector end
