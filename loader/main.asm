dc3811c2f1ac0b3e2e3bbd753bf5d9550ab52285
procub3r <procub3r@proton.me>
2024-02-05 01:01:01 +0530

    Add error reporting and load dummy stage2

--- loader/main.asm
+++ loader/main.asm
@@ -21,6 +21,11 @@ _start:
     jmp 0x0000:.reset_cs
 
 .reset_cs:
+    ; don't continue if not booted from a hard disk
+    cmp dl, 0x80
+    mov al, 'D' ; disk error
+    jne error
+
     ; set segment registers to 0
     xor si, si
     mov ds, si
@@ -31,7 +36,67 @@ _start:
     mov bp, 0x7c00
     mov sp, bp
 
-    jmp $ ; halt
+    ; check if bios extensions are supported
+    mov ah, 0x41
+    mov bx, 0x55aa
+    int 0x13
+    mov al, 'E' ; extensions not supported
+    jc error
+
+; there is a 33K ish size limitation on stage2 because of
+; how it is being loaded. we won't be hitting the limit tho
+load_stage2:
+    ; ds:si = address of disk address packet
+    push 0
+    pop ds
+    mov si, stage2_dapack
+
+    ; calculate number of sectors to read
+    mov cx, stage2.end - stage2
+    shr cx, 9 ; divide by 512
+    inc cx    ; cx = number of sectors
+
+; read stage2 to memory sector by sector
+.loop:
+    mov ah, 0x42 ; bios read function (with LBA)
+    int 0x13
+    mov al, 'R' ; disk read error
+    jc error
+
+    ; each iteration reads 512 bytes of stage2,
+    ; increment destination offset by 512
+    add word [stage2_dapack.offset], 512
+    ; LBA of the next sector to be read
+    inc word [stage2_dapack.lba]
+    dec cx
+    jnz .loop
+
+    jmp 0x7e00 ; jump to stage2
+
+; print error code in al to screen and hang
+error:
+    push 0xb800
+    pop es
+    mov ah, 0x0c
+    mov word [es:0], ax
+    jmp $
+
+; disk address packet structure for stage2
+align 4 ; must be 4 byte aligned
+stage2_dapack:
+.size:    db 16 ; size of the packet in bytes
+.zero:    db 0  ; unused, should be zero
+.sectors: dw 1  ; read one sector. int 0x13 will be called
+                ; in a loop to load the entirety of stage2
+.offset:  dw 0x0000
+.segment: dw 0x07e0 ; load sector to .segment:.offset
+.lba:     dq 0x0001 ; LBA of stage2 (= stage2 >> 9)
 
 times 510 - ($ - $$) db 0
 dw 0xaa55 ; bootsector end
+
+stage2:
+    times 4096 nop ; test multi-sector load
+    mov al, 'S' ; stage2 load success!
+    jmp error
+.end:
