604468371445307645ff38f6222a788049cad49a
procub3r <procub3r@proton.me>
2024-02-05 12:54:38 +0530

    Load GDT for long mode

--- loader/main.asm
+++ loader/main.asm
@@ -69,6 +69,11 @@ load_stage2:
     dec cx
     jnz .loop
 
+load_gdt:
+    ; load gdt for long mode
+    lgdt [gdtr]
+
+adios_stage1:
     jmp 0x7e00 ; jump to stage2
 
 ; print error code in al to screen and hang
@@ -78,6 +83,31 @@ error:
     mov ah, 0x0c
     mov word [es:0], ax
     jmp $
+
+; gdt register contents
+gdtr:
+    dw gdt.end - gdt - 1 ; size of gdt
+    dq gdt ; pointer to gdt
+
+; 64 bit global descriptor table
+gdt:
+    dq 0x00 ; first entry is a null descriptor
+    ; code segment descriptor
+    dw 0x0000     ; limit
+    dw 0x0000     ; base lo
+    db 0x00       ; base mid
+    db 0b10011010 ; access
+    db 0b10100000 ; flags and limit hi(?)
+    db 0x00       ; base hi
+
+    ; data segment descriptor
+    dw 0x0000     ; limit
+    dw 0x0000     ; base lo
+    db 0x00       ; base mid
+    db 0b10010010 ; access
+    db 0b11000000 ; flags and limit hi(?)
+    db 0x00       ; base hi
+.end:
 
 ; disk address packet structure for stage2
 align 4 ; must be 4 byte aligned
