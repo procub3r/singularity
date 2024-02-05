47211c4477cbd2f05458232b8d295e2263db6c22
procub3r <procub3r@proton.me>
2024-02-05 13:21:10 +0530

    Setup paging and jump to long mode!

--- loader/main.asm
+++ loader/main.asm
@@ -73,8 +73,58 @@ load_gdt:
     ; load gdt for long mode
     lgdt [gdtr]
 
-adios_stage1:
-    jmp 0x7e00 ; jump to stage2
+setup_paging:
+    ; zero out all entries on all levels
+    xor ax, ax     ; mov 0
+    mov cx, 0x4000 ; into 0x4000 bytes
+    mov di, 0x1000 ; starting from address 0x1000.
+    cld            ; increment di while looping
+    rep stosb
+
+    mov eax, 0x1000 ; address of PML4
+    mov cr3, eax    ; must be set in cr3.
+                    ; you can't mov it directly though
+
+    ; identity map the first G
+    mov word [0x1000], 0x2003 ; PML4[0] -> PDPT[0]
+    mov word [0x2000], 0x3003 ; PDPT[0] -> PD[0]
+    mov word [0x3000], 0x4003 ; PD[0] -> PT[0]
+
+    ; also map a G from -2G to the same paddrs as the first G
+    mov word [0x1ff8], 0x2003 ; PML4[-1] -> PDPT[0]
+    mov word [0x2ff0], 0x3003 ; PDPT[-2] -> PD[0]
+
+    ; indentity map the first 512 * 4 KiB = 2048 KiB
+    mov ebx, 0x0003 ; addr 0x0000 or'd with flags 0b11.
+                    ; 0b11 = page present + read/write perms
+    mov cx, 512     ; loop 512 times
+    mov di, 0x4000  ; address of PT
+
+.loop:
+    mov dword [di], ebx
+    add ebx, 0x1000 ; next page
+    add di, 8       ; size of each entry is 8 bytes
+    dec cx
+    jnz .loop
+
+    ; enable PAE (Physical Address Extension)
+    mov eax, cr4   ; read from cr4
+    or eax, 1 << 5 ; set PAE bit
+    mov cr4, eax   ; write to cr4
+
+    ; switch to long mode
+    mov ecx, 0xc0000080 ; EFER MSR
+    rdmsr               ; read from it
+    or eax, 1 << 8      ; set LM bit
+    wrmsr               ; write back to it
+
+    ; enable protection and paging
+    mov eax, 0x80000001 ; set PE and PG bits
+    mov cr0, eax        ; in cr0
+
+    ; set cs to 0x08 (offset of code segment descriptor in gdt)
+    ; and jump to stage2!
+    jmp 0x08:stage2
 
 ; print error code in al to screen and hang
 error:
@@ -123,8 +173,9 @@ stage2_dapack:
 times 510 - ($ - $$) db 0
 dw 0xaa55 ; bootsector end
 
+bits 64
 stage2:
     times 4096 nop ; test multi-sector load
-    mov al, 'S' ; stage2 load success!
-    jmp error
+    mov word [0xb8000], 0x0a00 | 'S'
+    jmp $
 .end:
